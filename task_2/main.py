1854e8cc81ae556f0b9ae58c9aef61d3c4d9a154
tottopyou <rgrabovsky2006@gmail.com>
2024-02-04 15:47:18 +0100

    the 2 part of 2 task

--- task_2/main.py
+++ task_2/main.py
@@ -4,14 +4,16 @@ info_2 = info.split('\n')
 
 forward = 0
 down = 0
+depth = 0
 
 for pair in info_2:
     parts = pair.split()
     if parts[0] == 'forward':
+        depth += int(parts[1]) * down
         forward += int(parts[1])
     elif parts[0] == 'down':
         down += int(parts[1])
     elif parts[0] == 'up':
         down -= int(parts[1])
 
-print(forward * down)
+print(forward * depth)
