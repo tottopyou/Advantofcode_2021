5c047741f66c58b251dafc09102b0fe1d5f4ab3c
tottopyou <rgrabovsky2006@gmail.com>
2024-02-04 14:33:33 +0100

    the 2 part of 1 task

--- task_1/main.py
+++ task_1/main.py
@@ -2,9 +2,13 @@ from data import *
 info_2 = info.split()
 
 res = 0
+pairs = []
 
-for i in range(1,len(info_2)):
-    if int(info_2[i]) > int(info_2[i-1]):
+for i in range(0,len(info_2)-3):
+    pairs.append(int(info_2[i])+int(info_2[i+1])+int(info_2[i+2]))
+
+for i in range(1,len(pairs)):
+    if int(pairs[i]) > int(pairs[i-1]):
         res += 1
 
-print(res)
+print(res+1)
