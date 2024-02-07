8ec335a37a0e7de3761be7c5be3102fb93adde85
tottopyou <rgrabovsky2006@gmail.com>
2024-02-07 16:14:15 +0100

    the 1 part of 7 task

---
+++ task_7/main.py
@@ -0,0 +1,25 @@
+from data import *
+
+tot = values.split(',')
+
+total = [int(x) for x in tot]
+
+trips = {}
+
+cost = []
+
+y = 0
+
+for i in range(max(total)):
+    for j ,price in enumerate(total):
+        x = abs(price - i)
+        while x != 0:
+            y += x
+            x -= 1
+        cost.append(y)
+        y = 0
+        if j == len(total)-1:
+            trips[i] = sum(cost)
+            cost = []
+
+print(min(trips.values()))
